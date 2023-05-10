a39ab7667a86c4e92122c6d1dd88e494c4198aef
Pierre Nédélec <pierrenedelec46@gmail.com>
2023-05-10 21:34:34 +0000

    update backoffice

--- core/admin.py
+++ core/admin.py
@@ -2,33 +2,67 @@ from django.contrib import admin
 from djangoql.admin import DjangoQLSearchMixin
 from import_export.admin import ImportExportModelAdmin
 
-from core.models import Level, Category, Skill, Country, Developer
+from core.models import Level, Category, Skill, Country, Developer, Config, Status, DeveloperSkill, Address
 
 
 @admin.register(Level)
 class LanguagesAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    list_editable = ["name", ]
+    list_display_links = None
 
 
 @admin.register(Category)
 class KindAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    list_editable = ["name", ]
+    list_display_links = None
 
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    list_editable = ["name", ]
+    list_display_links = None
 
 
 @admin.register(Country)
-class NiveauAdmin(admin.ModelAdmin):
+class CountryAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    list_editable = ["name", ]
+    list_display_links = None
 
 
-# Register your models here.
 @admin.register(Developer)
 class DeveloperAdmin(DjangoQLSearchMixin, ImportExportModelAdmin):
-    search_fields = ('skills', )
-    list_display = ('name', 'email', 'phone', 'address', 'created_at', 'updated_at')
-    list_editable = ["email", ]
+    search_fields = ('skills__name',)
+    list_display = ('name', 'email', 'phone', 'address', 'status', 'competences', 'created_at', 'updated_at')
+    list_editable = ["email", "status", ]
     readonly_fields = ('created_at', 'updated_at')
+
+    def competences(self, obj):
+        #competence with level and experience
+        return DeveloperSkill.objects.filter(developer=obj).values_list('skill__name', 'level__name', 'experience')
+
+
+@admin.register(Config)
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value',)
+    list_editable = ["value", ]
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code',)
+    list_editable = ["code", ]
+
+
+@admin.register(DeveloperSkill)
+class DeveloperSkillAdmin(admin.ModelAdmin):
+    list_display = ('developer', 'skill', 'level', 'experience')
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'street', 'country')
+    list_editable = ["city", "street", "country"]
--- core/models.py
+++ core/models.py
@@ -15,6 +15,9 @@ class Country(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Address(models.Model):
     id = models.AutoField(primary_key=True)
@@ -23,15 +26,25 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, )
 
+    def __str__(self):
+        return self.name + " " + self.city + " " + self.street
+
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Config(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=True, null=True)
+    value = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Level(models.Model):
@@ -39,13 +52,18 @@ class Level(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     value = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Skill(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     code = models.CharField(max_length=255, blank=True, null=True)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name='skills', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='skills', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Status(models.Model):
@@ -53,35 +71,52 @@ class Status(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     code = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Developer(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     firstName = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=255, blank=True, null=True)
-    email = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
     fixTel = models.CharField(max_length=255, blank=True, null=True)
     whatApp = models.CharField(max_length=255, blank=True, null=True)
     otherCanal = models.CharField(max_length=255, blank=True, null=True)
-    photo = models.CharField(max_length=255, blank=True, null=True)
-    cv = models.CharField(max_length=255, blank=True, null=False, default='')
-    linkedin = models.CharField(max_length=255, blank=True, null=True)
-    instagram = models.CharField(max_length=255, blank=True, null=True)
-    facebook = models.CharField(max_length=255, blank=True, null=True)
-    github = models.CharField(max_length=255, blank=True, null=True)
-    gitlab = models.CharField(max_length=255, blank=True, null=True)
-    bitbucket = models.CharField(max_length=255, blank=True, null=True)
-    website = models.CharField(max_length=255, blank=True, null=True)
-    video = models.CharField(max_length=255, blank=True, null=True)
+    photo = models.FileField(max_length=255, blank=True, null=True)
+    cv = models.FileField(max_length=255, blank=True, null=False, default='')
+    linkedin = models.URLField(max_length=255, blank=True, null=True)
+    instagram = models.URLField(max_length=255, blank=True, null=True)
+    facebook = models.URLField(max_length=255, blank=True, null=True)
+    github = models.URLField(max_length=255, blank=True, null=True)
+    gitlab = models.URLField(max_length=255, blank=True, null=True)
+    bitbucket = models.URLField(max_length=255, blank=True, null=True)
+    website = models.URLField(max_length=255, blank=True, null=True)
+    video = models.FileField(max_length=255, blank=True, null=True)
     tjm = models.CharField(max_length=255, blank=True, null=True)
-    annotations = models.CharField(max_length=255, blank=True, null=False)
+    annotations = models.TextField(blank=True, null=False)
     availability = models.CharField(max_length=255, blank=True, null=False)
     modeReglement = models.CharField(max_length=255, blank=True, null=False)
     bankAccount = models.CharField(max_length=255, blank=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True),
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True, auto_created=True),
     # relations
     status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='developers', blank=True, null=True)
     address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name='developer')
-    skills = models.ManyToManyField(Skill, related_name='developers', blank=True)
+    skills = models.ManyToManyField(Skill, through='DeveloperSkill', related_name='developer')
+
     # auth
+    def __str__(self):
+        return self.name
+
+
+class DeveloperSkill(models.Model):
+    id = models.AutoField(primary_key=True)
+    developer = models.ForeignKey(Developer, on_delete=models.CASCADE, related_name='developer_skills')
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='developer_skills')
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name='developer_skills')
+    experience = models.IntegerField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.developer.name + " " + self.skill.name + " " + self.level.name

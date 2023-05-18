72d83f84441b1702cc311b7d2163665b1754fd63
 esse.jacques.co <essedansomon@gmail.com>
2023-05-18 13:33:55 +0000

    STATICFILES_DIRS

--- ultra/settings.py
+++ ultra/settings.py
@@ -157,7 +157,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
-STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

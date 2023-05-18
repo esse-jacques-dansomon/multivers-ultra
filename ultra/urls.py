d6cc40f8318426262f34ecae7c7ee9dc66ff4a1d
 esse.jacques.co <essedansomon@gmail.com>
2023-05-18 14:46:05 +0000

    STATICFILES_DIRS

--- ultra/urls.py
+++ ultra/urls.py
@@ -42,5 +42,5 @@ urlpatterns = [
                   path('api/rest/developers/', DeveloperCreateView.as_view(), name='developers'),
               ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
